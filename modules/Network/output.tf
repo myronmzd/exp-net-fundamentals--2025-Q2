4f43d9b9cce7eca7800c0a783ea21a5b525999ab
Myron <myronmzd22@gmail.com>
2025-06-09 17:01:31 +0000

    v

--- modules/Network/output.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  description = "VPC ID"
-  value       = aws_vpc.main.id  # ✅ Directly reference the local resource
-}
-
-output "subnet_id" {
-  description = "Subnet ID"
-  value       = aws_subnet.main.id  # ✅ Directly reference the local resource
-}
